5b1f2df3a7e8751d2377e39ecc3286d5b6281514
vPartyzan <peregrim.mt@gmail.com>
2021-06-09 14:19:55 +0200

    Add check image, characters in title, author and email in photos.controller.js

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -8,12 +8,37 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    const emailPattern = new RegExp('^[a-zA-Z0-9][a-zA-Z0-9_.-]+@[a-zA-Z0-9][a-zA-Z0-9_.-]+\.{1,3}[a-zA-Z]{2,4}')
+    const textPattern = new RegExp(/(\w|\s|\.)*/, 'g');
+  
+    const emailMatched = email.match(emailPattern).join('');
+    const titleMatched = title.match(textPattern).join('');
+    const authorMatched = author.match(textPattern).join('');
 
+    if (emailMatched.length < email.length && titleMatched.length < title.length && authorMatched.length < author.length) {
+      throw new Error('Wrong characters, try again!');
+    }
+
+    if (
+      titleMatched.length == title.length 
+      && authorMatched.length == author.length 
+      && emailMatched.length == email.length 
+      && title.length <= 25 
+      && author.length <= 50
+    ) { // if fields are not empty...
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
+
+      if (fileExt === 'jpg' || fileExt === 'jpeg' || fileExt === 'png' || fileExt === 'gif') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        console.log(fileExt);
+
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        res.json('Something wrong, try again..');
+      }
+      
 
     } else {
       throw new Error('Wrong input!');
